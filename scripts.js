5913fed8b78ee151c229ebb8b557ccc109c7d4ab
ablades <audaris@gmail.com>
2020-03-23 11:10:07 -0700

    add color and defaults to quadrant III

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ const widthValue = document.getElementById('cWidth')
 const widthDisplay = document.getElementById('wDisplay')
 const heightValue = document.getElementById('cHeight')
 const heightDisplay = document.getElementById('hDisplay')
+const colorValue = document.getElementById('cValue')
+const colorDisplay = document.getElementById('cDisplay')
+
+document.addEventListener("DOMContentLoaded", function() {
+    heightDisplay.innerHTML = heightValue.value + "px"
+    widthDisplay.innerHTML = widthValue.value + "px"
+    colorDisplay.innerHTML = colorValue.value 
+})
 
 widthValue.addEventListener("input", function(){
     widthDisplay.innerHTML = widthValue.value + "px"
@@ -9,4 +17,8 @@ widthValue.addEventListener("input", function(){
 
 heightValue.addEventListener("input", function(){
     heightDisplay.innerHTML = heightValue.value + "px"
+})
+
+colorValue.addEventListener("input", function (){
+    colorDisplay.innerHTML = colorValue.value
 })
