e1b783766966a7e670a0fb0a0c32ba83d2b08c49
ablades <audaris@gmail.com>
2020-03-23 11:39:59 -0700

    add quadrant 4

--- scripts.js
+++ scripts.js
@@ -4,21 +4,27 @@ const heightValue = document.getElementById('cHeight')
 const heightDisplay = document.getElementById('hDisplay')
 const colorValue = document.getElementById('cValue')
 const colorDisplay = document.getElementById('cDisplay')
+const colorBox = document.getElementById('cBox')
 
 document.addEventListener("DOMContentLoaded", function() {
     heightDisplay.innerHTML = heightValue.value + "px"
     widthDisplay.innerHTML = widthValue.value + "px"
-    colorDisplay.innerHTML = colorValue.value 
+    colorBox.style.backgroundColor = colorValue.value
+    colorBox.style.width = widthValue.value + "px"
+    colorBox.style.height = heightValue.value + "px"
 })
 
 widthValue.addEventListener("input", function(){
     widthDisplay.innerHTML = widthValue.value + "px"
+    colorBox.style.width = widthValue.value + "px"
 })
 
 heightValue.addEventListener("input", function(){
     heightDisplay.innerHTML = heightValue.value + "px"
+    colorBox.style.height = heightValue.value + "px"
 })
 
 colorValue.addEventListener("input", function (){
     colorDisplay.innerHTML = colorValue.value
+    colorBox.style.backgroundColor = colorValue.value
 })
